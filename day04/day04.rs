cdf8d4075924f3dac4b8533c6a4a27ef41f3eb12
Sofiane <sofiane.meftah@epita.fr>
2022-12-04 11:39:52 +0100

    Add day 4

--- day04/day04.rs
+++ day04/day04.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{Result, BufReader, BufRead}};
+use std::{fs::File, io::{Result, BufReader, BufRead}, ops::RangeInclusive};
 
 fn main() -> Result<()> {
     let f = File::open("input.txt")?;
@@ -16,10 +16,33 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn parse_range(rg: &str) -> RangeInclusive<usize> {
+    let (start, end) = rg.split_once("-").unwrap();
+    RangeInclusive::new(start.parse().unwrap(), end.parse().unwrap())
+}
+
 fn part1(lines: &[String]) -> Result<String> {
-    todo!("Part 1")
+    let mut count = 0;
+    for line in lines {
+        let (elfe_a, elfe_b) = line.split_once(",")
+            .map(|(a, b)| (parse_range(&a), parse_range(&b))).unwrap();
+        if (elfe_a.contains(elfe_b.start()) && elfe_a.contains(elfe_b.end()))
+        || (elfe_b.contains(elfe_a.start()) && elfe_b.contains(elfe_a.end())) {
+            count += 1;
+        }
+    }
+    Ok(format!("Full Overlaps = {}", count))
 }
 
 fn part2(lines: &[String]) -> Result<String> {
-    todo!("Part 2")
+    let mut count = 0;
+    for line in lines {
+        let (elfe_a, elfe_b) = line.split_once(",")
+            .map(|(a, b)| (parse_range(&a), parse_range(&b))).unwrap();
+        if elfe_a.contains(elfe_b.start()) || elfe_a.contains(elfe_b.end())
+        || elfe_b.contains(elfe_a.start()) || elfe_b.contains(elfe_a.end()) {
+            count += 1;
+        }
+    }
+    Ok(format!("Overlaps = {}", count))
 }
