f176c390e868a47efe73602f4838fd2681bcf55c
Sofiane <sofiane.meftah@outlook.fr>
2022-12-06 09:49:43 +0100

    Add day 6

--- day06/day06.rs
+++ day06/day06.rs
@@ -16,10 +16,45 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn is_distinct(packet: &[char]) -> bool {
+    for i in 0..packet.len() {
+        for j in (i + 1)..packet.len() {
+            if packet[i] == packet[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn part1(lines: &[String]) -> Result<String> {
-    todo!("Part 1")
+    let mut starts = vec![];
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect();
+        let mut idx = 0;
+        for packet in chars.windows(4) {
+            if is_distinct(packet) {
+                starts.push(idx + 4);
+                break;
+            }
+            idx += 1;
+        }
+    }
+    Ok(format!("{:?}", starts))
 }
 
 fn part2(lines: &[String]) -> Result<String> {
-    todo!("Part 2")
+    let mut starts = vec![];
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect();
+        let mut idx = 0;
+        for packet in chars.windows(14) {
+            if is_distinct(packet) {
+                starts.push(idx + 14);
+                break;
+            }
+            idx += 1;
+        }
+    }
+    Ok(format!("{:?}", starts))
 }
