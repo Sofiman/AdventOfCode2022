f1167475bd2c6152d0e23b504f6d94fff23682a9
Sofiane <sofiane.meftah@epita.fr>
2022-12-12 22:19:02 +0100

    Add code for day 9

--- day09/day09.rs
+++ day09/day09.rs
@@ -69,5 +69,64 @@ fn part1(lines: &[String]) -> Result<String> {
 }
 
 fn part2(lines: &[String]) -> Result<String> {
-    todo!("Part 2")
+    let mut segments: Vec<(i32, i32)> = vec![(0, 5); 10];
+    let mut visited = HashSet::new();
+    visited.insert(segments[0]);
+    for line in lines {
+        let (kind, amount) = line.split_once(" ").unwrap();
+        let mut amount: u16 = amount.parse().unwrap();
+        //println!("== {} ==", line);
+        while amount > 0 {
+            let mut head = &mut segments[0];
+            match kind {
+                "U" => head.1 -= 1,
+                "L" => head.0 -= 1,
+                "D" => head.1 += 1,
+                "R" => head.0 += 1,
+                _ => unreachable!("Unknown direction")
+            }
+            for i in 1..segments.len() {
+                let seg = segments[i - 1];
+                let mut tail = &mut segments[i];
+                let dx = seg.0 - tail.0;
+                let dy = seg.1 - tail.1;
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    if dx != 0 {
+                        tail.0 += dx / dx.abs();
+                    }
+                    if dy != 0 {
+                        tail.1 += dy / dy.abs();
+                    }
+                }
+            }
+            visited.insert(segments[segments.len() - 1]);
+
+            /*
+            for i in 0..6 {
+                for j in 0..6 {
+                    if segments[0].0 == j && segments[0].1 == i {
+                        print!("H");
+                    } else {
+                        let mut found = false;
+                        for (k, seg) in segments[1..].iter().enumerate() {
+                            if seg.0 == j && seg.1 == i {
+                                found = true;
+                                print!("{}", k + 1);
+                                break;
+                            }
+                        }
+                        if !found {
+                            print!(".");
+                        }
+                    }
+                }
+                println!();
+            }
+            println!();
+            */
+
+            amount -= 1;
+        }
+    }
+    Ok(format!("total visited: {}", visited.len()))
 }
